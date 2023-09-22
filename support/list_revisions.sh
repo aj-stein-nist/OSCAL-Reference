82d293d9bf43a313a091f633b52e671529fbc8a0
A.J. Stein <alexander.stein@nist.gov>
2023-09-22 11:29:01 -0400

    Adjust revision script for prototype branches for #19.

--- support/list_revisions.sh
+++ support/list_revisions.sh
@@ -15,4 +15,14 @@ TAGGED_REVISIONS=$(echo "${TAGS}" | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$')
 # sort tags (which should be in semver-ish format)
 TAGGED_REVISIONS=$(echo "${TAGGED_REVISIONS}" | sort -t "." -k1,1n -k2,2n -k3,3n)
 
-echo "${TAGGED_REVISIONS}"
+# retrieve prefix for branches of prototypes to be listed and published and if
+# it is empty, it can be ignored.
+PROTOTYPE_BRANCHES_REMOTE=${PROTOTYPE_BRANCHES_REMOTE:-"origin"}
+PROTOTYPE_BRANCHES_PREFIX=${PROTOTYPE_BRANCHES_PREFIX:-"prototype"}
+# the output of ls-remote is the best for this but doesn't have ready-made
+# format or pretty-print options. We need to use sed to filter the output
+# output is the following format:
+# sha1-hash        refs/heads/branch-name
+# 012345abcd012345abcd012345abcd012345abcd        refs/heads/branch-name
+PROTOTYPE_BRANCHES=$(cd "${OSCAL_DIR}"; git ls-remote "${PROTOTYPE_BRANCHES_REMOTE}" "${PROTOTYPE_BRANCHES_PREFIX}*" | sed -n -e "s|^.*\(refs/heads/\)\(${PROTOTYPE_BRANCHES_PREFIX}\)|\2|p")
+echo "${TAGGED_REVISIONS}" "${PROTOTYPE_BRANCHES}"
